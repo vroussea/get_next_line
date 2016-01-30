9ae3d4fdd92fbf05948ef1daec9c6c2270e6ad81
Valentin ROUSSEAU <vroussea@e1r2p6.42.fr>
2016-01-30 21:43:15 +0100

    nearly working, true this time

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: vroussea <vroussea@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/01/05 22:07:54 by vroussea          #+#    #+#             */
-/*   Updated: 2016/01/30 19:35:55 by vroussea         ###   ########.fr       */
+/*   Updated: 2016/01/30 21:24:03 by vroussea         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,24 +25,13 @@ int	get_next_line(int const fd, char **line)
 	while (!ft_strchr(str, '\n') && ret > 0)
 	{
 		ret = read(fd, buff, BUFF_SIZE);
-		ft_putstr("ret : ");
-		ft_putendl(ft_itoa(ret));
-		ft_putendl(buff);
 		if (ret == -1)
 			return (-1);
 		if (ret > 0)
 			ft_strncat(str, buff, ft_strchr(buff, '\n') - buff + 1);
 	}
 	i = 0;
-	ft_putendl("str existe");
-	while (str[i] != '\0')
-	{
-		ft_putchar(str[i]);
-		i++;
-	}
-	ft_putendl("str existe");
 	*line = (!ft_strchr(str, '\n') ? ft_strsub(str, 0, ft_strlen(str)) :
 			ft_strsub(str, 0, ft_strchr(str, '\n') - str));
-	ft_putendl("test");
 	return (ret ? 1 : 0);
 }
--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: vroussea <vroussea@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/01/27 17:39:40 by vroussea          #+#    #+#             */
-/*   Updated: 2016/01/30 17:08:12 by vroussea         ###   ########.fr       */
+/*   Updated: 2016/01/30 21:39:35 by vroussea         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,9 @@
 
 int	main(int argc, char **argv)
 {
-	int		fd;
+	static int		fd;
 	int		result;
-	char	**line;
+	char	*line;
 
 	if (argc == 2)
 	{
@@ -25,13 +25,16 @@ int	main(int argc, char **argv)
 		fd = open(argv[1], O_RDONLY);
 		while (result == 1)
 		{
-			result = get_next_line(fd, line);
-			ft_putendl("test");
-			ft_putendl(*line);
+			result = get_next_line(fd, &line);
+			ft_putstr("result : ");
+			ft_putendl(ft_itoa(result));
+			ft_putendl(line);
 		}
 		if (result == -1)
 			ft_putendl("error");
 		close(fd);
 	}
+	else
+		ft_putendl("error");
 	return (0);
 }
